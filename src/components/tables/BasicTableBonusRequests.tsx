9ff8414d6fe9efafcfb6991a4799cd7742726716
leonardoelit <leonardoelitgold@gmail.com>
2025-09-21 14:18:35 +0300

    Finished freespin shit

--- src/components/tables/BasicTableBonusRequests.tsx
+++ src/components/tables/BasicTableBonusRequests.tsx
@@ -431,8 +431,8 @@ const BasicTableBonusRequests = () => {
                     <TableCell className={`px-4 py-3 ${t.status === 0 || t.status === 1 ? 'text-yellow-600' : t.status === 2 ? 'text-success-600' : t.status === 3 ? 'text-red-600' : 'text-gray-500 dark:text-gray-400'} text-start text-theme-sm`}>
                       {bonusRequestStatusEnum.find((p) => p.id === t.status)?.name}
                     </TableCell>
-                    <TableCell title={t.note} className="px-3 py-2 text-gray-900 truncate max-w-[120px]">
-                      {t.note}{" "}{t.freespinWinAmount && t.freespinWinAmount.toLocaleString()}
+                    <TableCell title={t.note} className="px-3 py-2 text-gray-900 text-start text-theme-sm truncate max-w-[120px]">
+                      {t.note}{" "}{t.freespinWinAmount && `₺${t.freespinWinAmount.toLocaleString()}`}
                     </TableCell>
                     <TableCell className="px-4 py-3 text-gray-900 text-start text-theme-sm dark:text-gray-400">
                       {t.updatedAt === undefined ? " - " : formatDateToDDMMYYYYHHMMSS(t.updatedAt)}
